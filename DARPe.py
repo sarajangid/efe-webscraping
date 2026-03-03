0844959fdba3d513cf3ee63f7885434a3155e332
Ethan Jang <eef.jang@berkeley.edu>
2026-03-02 21:55:15 -0800

    Update DARPe.py

--- DARPe.py
+++ DARPe.py
@@ -1,6 +1,10 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = "http://darpe.me/tenders-and-grants/"
 page = requests.get(URL)
+# print(page.text)
+soup = BeautifulSoup(page.content, "html.parser")
 
-print(page.text)
+grant_cards = soup.find("td", style="border-right:0px")
+print(grant_cards.prettify())
